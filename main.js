a56524b9d652c2cb3ffbb59df80bb9a4396e21c1
Ureeces <Ulysses.Reyes@codeimmersives.com>
2020-07-16 12:09:19 -0400

    Pulls from csv and stores into 2D array. Takes in input from user and print outs correct format.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // All imports/required functions
 const fs = require('fs');
 const getRows = require('./get-rows.js');
+const getTotalCases = require('./get-total-cases-by-day.js');
+const getNewCases = require('./get-new-cases-by-day.js');
 
 // Goal
 // Get input from user and store it in an array
@@ -14,9 +16,9 @@ const getRows = require('./get-rows.js');
 // Solution
 // 1. Pull and read data from our database(or in this case, csv file)
 // 2. Transform our data into a 2D array using our getRows function
-// 3. Store user input into an array to account for different
-//    commands
+// 3. Store user input into an array to account for different commands
 // 4. Get the command from user input. This should be at index 2.
+// 4a. If there is no command, inform user of correct input
 // 5. Get the date from user input. This should be at index 3.
 // 5a. If there is no date, inform user that there must be a date entered.
 // 6. Check the command slot
@@ -25,6 +27,37 @@ const getRows = require('./get-rows.js');
 // 6c. Any other command is invalid, and we should inform user of
 //     incorrect input and show proper format.
 
+// 1. Pull and read data from our database(or in this case, csv file)
+// 2. Transform our data into a 2D array using our getRows function
+const covidStrData = fs.readFileSync('./owid-covid-usa-data.csv', 'utf-8');
+const covidTableData = getRows(covidStrData);
+
+
+// 3. Store user input into an array to account for different commands
+const userInput = process.argv;
+
+// Message(s) for error handling
+const correctFormatMessage = "Correct format is node main.js [command] [date]";
+
+// 4. Get the command from user input. This should be at index 2.
+// 4a. If there is no command, inform user of correct input
+const userCommand = userInput[2];
+if(userCommand === undefined) {
+    throw new Error(correctFormatMessage);
+}
+
+// 5. Get the date from user input. This should be at index 3.
+// 5a. If there is no date, inform user that there must be a date entered.
+const userDate = userInput[3];
+if(userDate === undefined) {
+    throw new Error(correctFormatMessage);
+}
+
+// 6. Check the command slot
+// 6a. If command is "total" - display the total cases for the date entered
+// 6b. If command is "new" - display the new cases for the date entered.
+// 6c. Any other command is invalid, and we should inform user of
+//     incorrect input and show proper format.
 
 
 
